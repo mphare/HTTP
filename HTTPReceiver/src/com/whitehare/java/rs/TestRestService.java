6494a2fd2ddbac1907005fcd23b521caff3604ec
Michael Hare <mphare-dev@whitehare.com>
2016-10-15 10:47:26 -0500

    UPD-0001 Added a second endpoint with a plain text result

--- HTTPReceiver/src/com/whitehare/java/rs/TestRestService.java
+++ HTTPReceiver/src/com/whitehare/java/rs/TestRestService.java
@@ -5,11 +5,13 @@ import java.io.InputStream;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/test")
 public class TestRestService
 {
   @GET
+  @Path("/verify")
   @Produces("application/xml")
   public String verifyEndpoint(InputStream incomingData)
   {
@@ -20,4 +22,15 @@ public class TestRestService
 
   }
 
+  @GET
+  @Path("/verify2")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String verifyEndpoint2(InputStream incomingData)
+  {
+    String result = "The 2nd Test Endpoint was Hit";
+
+    return "<ctofservice>" + "<celsius>" + "ABC123" + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>"
+        + "</ctofservice>";
+
+  }
 }
