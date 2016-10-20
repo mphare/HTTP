5636fda97dc0ef5a2d0086caa2d288bf802caf01
Michael Hare <mhare@zixcorp.com>
2016-10-20 12:24:26 -0500

    UPD-0001: Added simple Example JSP, moved web app dir to /web/*

---
+++ HTTPReceiver/src/com/whitehare/httpreceiver/test/WhenUsingDatabase.java
@@ -0,0 +1,24 @@
+package com.whitehare.httpreceiver.test;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class WhenUsingDatabase
+{
+
+  @Test
+  public void shouldWriteDate()
+  {
+    String cardnumber = "987654321";
+    String classroom = "210";
+    Date swipeTime = new Date();
+    /*
+     * CardSwipe cs = new CardSwipe(cardnumber, classroom, swipeTime);
+     * 
+     * CreateCardSwipe ccs = new CreateCardSwipe(); long ret =
+     * ccs.saveCardSwipe(cs);
+     */
+  }
+
+}
